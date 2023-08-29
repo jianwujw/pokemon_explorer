db8a969ebb5e36bc8efba79505acdc2304817c4d
jianwujw <142988241+jianwujw@users.noreply.github.com>
2023-08-28 20:17:08 -0700

    api calls to fill database
    added init.py for package
    
    pagination loop to see how many pokemons there are in the API
    
    work in progress for custom command import

---
+++ management/commands/import_pokemon_data.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from pokemons_zbottom.models import *
+import requests
+
+class Comamnd(BaseCommand):
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.SUCCESS('Custom command executed successfully'))
+    
+    def pokemon_call():
+        pokemon_count = 1281
+        for pokemon in range(1,pokemon_count):
+            url = f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
+            response = requests.get(url)
+
+            if response.status_code ==200:
+                data= response.json()
+                Pokemon.objects.create(name = data['name'])
+                
+            else:
+                print("Request failed", response.status_code)
+            
---
+++ management/commands/pagination_loop_api.py
@@ -0,0 +1,24 @@
+import requests
+
+page = 1
+batch = 20
+num_pokemon = 1
+
+while True:
+    url = f'https://pokeapi.co/api/v2/pokemon/?offset={(page-1)*batch}&limit={batch}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        pokemons = data['results']
+
+        if not pokemons:
+            break
+
+        for pokemon in pokemons:
+            print(num_pokemon ,pokemon['name'],page)
+        page +=1
+        num_pokemon+=1
+    else:
+        print("nothing",response.status_code)
+        break
