774a128c9250469401d244245f09ea9d8cc5b7ba
jianwujw <142988241+jianwujw@users.noreply.github.com>
2023-09-08 17:43:41 -0700

    filtered moves to ascending order

--- pokemons_zbottom/views.py
+++ pokemons_zbottom/views.py
@@ -6,8 +6,14 @@ from .models import *
 
 
 def index(request):
+    
     pokemons = Pokemon.objects.all()
-    return render(request,'index.html', {'pokemons':pokemons})
+    ordered_moves = Moves.objects.all().order_by('level_learned')
+    for pokemon in pokemons:
+        ordered_moves =pokemon.moves.order_by('level_learned')
+
+
+    return render(request,'index.html', {'pokemons':pokemons,'ordered_moves':ordered_moves})
 
 
 # Create your views here.
