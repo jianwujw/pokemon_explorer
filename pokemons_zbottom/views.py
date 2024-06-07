daf7f12e8c4c2aa8e540658ecd7045c652ba8bf1
jianwujw <142988241+jianwujw@users.noreply.github.com>
2024-06-06 20:28:23 -0700

    home page grid change
    fixed formatting of home page
    added info page when clicking on pokemon
    sorted by level in which the pokemon can learn the move

--- pokemons_zbottom/views.py
+++ pokemons_zbottom/views.py
@@ -15,5 +15,13 @@ def index(request):
 
     return render(request,'index.html', {'pokemons':pokemons,'ordered_moves':ordered_moves})
 
+def info(request, id):
+    pokemon = Pokemon.objects.get(id=id)
+    #sort our moves by level learned using lambda
+    sortedmoves = sorted(pokemon.moves.all(),key =lambda x: x.level_learned)
+    return render(request,'info.html',{'pokemon':pokemon,'sortedmoves':sortedmoves})
+
+
+
 
 # Create your views here.
