d8556d8df63a3ae6312b44c7455d4416ee2cff1e
jianwujw <142988241+jianwujw@users.noreply.github.com>
2024-06-06 17:35:57 -0700

    added static file path and favicon.ico

--- pokemons_zbottom/management/commands/display_db_info.py
+++ pokemons_zbottom/management/commands/display_db_info.py
@@ -6,8 +6,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         db = Pokemon.objects.all()
         
-        for stuff in db:           
-            self.stdout.write(self.style.SUCCESS(f'ID: {stuff.id}, Name: {stuff.name} Moves: {", ".join(move.name for move in stuff.moves.all())}'))
+        for data in db:           
+            self.stdout.write(self.style.SUCCESS(f'ID: {data.id}, Name: {data.name} Moves: {", ".join(move.name for move in data.moves.all())}'))
 
 
         
--- pokemons_zbottom/tests.py
+++ pokemons_zbottom/tests.py
@@ -1,3 +1,5 @@
 from django.test import TestCase
+from .models import Pokemon
 
 # Create your tests here.
+
