f8e740e02c684cd99f455221a531ae4744ffd0e4
jianwujw <142988241+jianwujw@users.noreply.github.com>
2025-03-05 08:36:12 -0800

    branch merge to main

--- pokemons_zbottom/management/commands/delete_db.py
+++
@@ -1,6 +0,0 @@
-from django.core.management import BaseCommand
-from pokemons_zbottom.models import Pokemon
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        Pokemon.objects.all().delete()
--- pokemons_zbottom/views.py
+++ pokemons_zbottom/views.py
@@ -15,5 +15,21 @@ def index(request):
 
     return render(request,'index.html', {'pokemons':pokemons,'ordered_moves':ordered_moves})
 
+def info(request, id):
+    pokemon = Pokemon.objects.get(id=id)
+    #sort our moves by level learned using lambda
+    sortedmoves = sorted(pokemon.moves.all(),key =lambda x: x.level_learned)
+    machine_tutor_egg=[]
+    levelup=[]
+    for x in sortedmoves:
+        if x.move_learn_method.name != 'level-up':
+            machine_tutor_egg.append(x)
+        else:
+            levelup.append(x)
+   
+    return render(request,'info.html',{'pokemon':pokemon,'machine_tutor_egg':machine_tutor_egg,'levelup':levelup})
+
+
+
 
 # Create your views here.
